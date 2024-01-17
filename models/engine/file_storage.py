874b2bee745d09ced3c98c97abf3c3c84157128c
Gmukoro <gmukoro3@gmail.com>
2024-01-17 14:43:37 +0000

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,31 +8,51 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None) -> dict:
+        """Returns a dictionary of models of specified class
+        current storage
+        """
+        from models.base_model import Base
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+        from models.user import User
+
+        if cls:
+            cls_dict = {}
+            if isinstance(cls, str):
+                cls = eval(cls)
+            cls_name = cls.__name__
+            all_objs = FileStorage.__objects
+            for key, val in all_objs.items():
+                if key.split(".")[0] == cls_name:
+                    cls_dict.update({key: val})
+            return cls_dict
         return FileStorage.__objects
 
+    def delete(self, obj=None):
+        """Deletes object instance from local storage FileStorage.__objects"""
+        if obj is None:
+            return
+        obj_key = "{}.{}".format(type(obj).__name__, obj.id)
+        del FileStorage.__objects[obj_key]
+        self.save()
+
+
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        # self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
-        # """Adds instance to objects_dict with key <instance_class_name>.id."""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """Adds new object to storage dictionar"""
+        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
 
     def save(self):
-        #"""Saves storage dictionary to file"""
-        #with open(FileStorage.__file_path, 'w') as f:
-        #    temp = {}
-        #    temp.update(FileStorage.__objects)
-        #    for key, val in temp.items():
-        #        temp[key] = val.to_dict()
-        #   json.dump(temp, f)
-
-        """Serializes objects_dict to the JSON file file_path."""
-        key_val = self.__objects.items()
-        serialized_objects = {key: val.to_dict() for key, val in key_val}
-        with open(self.__file_path, "w") as file:
-            json.dump(serialized_objects, file)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, "w") as f:
+            emp = {}
+            emp.update(FileStorage.__objects)
+            for key, val in emp.items():
+                emp[key] = val.to_dict()
+            json.dump(emp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
