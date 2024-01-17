fab020fd01724cec8e98edf391066d164a4aff56
root <tefeglory.official@gmail.com>
2024-01-18 00:38:26 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -5,11 +5,11 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = "users"
+
     first_name = Column(String(128))
     last_name = Column(String(128))
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
