e0caba9ddf1a03a8b09fbe9891f8938699357114
Gmukoro <gmukoro3@gmail.com>
2024-01-18 03:00:43 +0000

    place.py

--- models/place.py
+++ models/place.py
@@ -38,19 +38,19 @@ place_amenity = Table(
 class Place(BaseModel, Base):
     """ A place to stay """
 
-    __tablename__ = "places"
+     __tablename__ = "places"
+
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
-    reviews = relationship("Review", backref="place", cascade="delete")
 
 
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
