ae44f403896be79ab84aa4edda15feb69b25044d
Gmukoro <gmukoro3@gmail.com>
2024-01-18 15:38:17 +0000

    place.py

--- models/place.py
+++ models/place.py
@@ -1,12 +1,23 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.review import Review
-from models.user import User
-from models.amenity import Amenity
-from os import getenv
-from sqlalchemy import Column, String, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
-from sqlalchemy import MetaData
-import models
+import shlex
+import os
+from os import getenv
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                     Column('place_id', String(60),
+                            ForeignKey('places.id'),
+                            primary_key=True,
+                            nullable=False),
+                     Column('amenity_id', String(60),
+                            ForeignKey('amenities.id'),
+                            primary_key=True,
+                            nullable=False)
+                     )
 
 class Place(BaseModel, Base):
     """A place to stay"""
