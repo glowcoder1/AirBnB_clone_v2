2092f947ff189f3f0e6786e3e132a6409e4945b9
Gmukoro <gmukoro3@gmail.com>
2024-01-18 14:32:50 +0000

    place.py

--- models/place.py
+++ models/place.py
@@ -1,17 +1,19 @@
-#!/usr/bin/python3
-""" Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.review import Review
 from models.user import User
 from models.amenity import Amenity
 from os import getenv
-import models
 from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from sqlalchemy import MetaData
+import models
 
 metadata = MetaData()
 
+"""
+Table defining the Many-to-Many relationship between
+places and amenities
+"""
 place_amenity = Table('place_amenity', metadata,
                      Column('place_id', String(60),
                             ForeignKey('places.id'),
@@ -24,25 +26,23 @@ place_amenity = Table('place_amenity', metadata,
                      )
 
 
-class Place(BaseModel):
-    """ A place to stay """
-
+class Place(BaseModel, Base):
+    """A place to stay"""
     if getenv("HBNB_TYPE_STORAGE") == "db":
         __tablename__ = "places"
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
 
         reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
-
-        amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
 
     else:
         city_id = ""
