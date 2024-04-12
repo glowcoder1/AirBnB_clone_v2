73a31e3383993631216f2b14068c3fc45f0098e2
root <tefeglory.official@gmail.com>
2024-04-12 21:05:00 +0100

    task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""
+   fabric script that generates a .tgz archive
+   from the contents of the web_static
+"""
+
+import os
+from datetime import datetime
+from fabric.api import local, run, put, env
+
+
+env.hosts = ["100.26.244.115", "52.206.162.79"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Archives the static files and returns path if successful."""
+    local("mkdir -p versions")
+    d_time = datetime.now()
+    curr_version = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
+    try:
+        local("tar -cvzf {} web_static".format(curr_version))
+    except Exception:
+        curr_version = None
+    return curr_version
+
+
+def do_deploy(archive_path):
+    """distributes an archive to web servers"""
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        archived_file = archive_path.split("/")[-1]
+        uncompressed = "/data/web_static/releases/{}".format(archive_path
+                                                             .split(".")[0])
+        archived_file = "/tmp/{}".format(archived_file)
+
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(uncompressed))
+        run("sudo tar -xzf {} -C {}/".format(archived_file, uncompressed))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(uncompressed,
+                                                uncompressed))
+        run("sudo rm -rf {}/web_static".format(uncompressed))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(uncompressed))
+
+        print("New version deployed!")
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """creates and distributes an archive to your web servers"""
+    archive_path =  do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
