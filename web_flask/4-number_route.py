398878e77b0a5b989ebe6d02e0b92bf518556234
root <tefeglory.official@gmail.com>
2024-05-16 22:02:06 +0100

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+starts a Flask application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """handles home path"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """handles /hbnb path"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_with_text(text):
+    """handles /c path"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def py_with_text(text):
+    """handles /python path"""
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    """handles /number path"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
