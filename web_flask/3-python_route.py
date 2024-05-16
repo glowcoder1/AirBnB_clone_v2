d568238dac9d919700cf844594c7ec0fb747b96b
root <tefeglory.official@gmail.com>
2024-05-16 21:56:58 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+starts a Flask application
+"""
+
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """handles home path"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """handles /hbnb path"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_with_text(text):
+    """handles /c path"""
+    return 'C {}'.format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def py_with_text(text):
+    """handles /python path"""
+    return 'Python {}'.format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
